166295c14f3d52e17f53620ac506aa316c3e8a00
DMOC1211 <145091883+DMOC1211@users.noreply.github.com>
2026-04-22 12:39:51 -0700

    Update sneakyspy.py

--- sneakyspy.py
+++ sneakyspy.py
@@ -4,7 +4,23 @@ Date: 4/22/26
 Group: 10
 Description:
 '''
+from spy import Spy
 
+
+class SneakySpy(Spy):
+    """Spy specializing in stealth and agility."""
+
+    def description(self):
+        return "Sneaky Spy"
+
+    def agility(self):
+        return 4
+
+    def stealth(self):
+        return 5
+
+    def tech_ability(self):
+        return 1
 
 
 
