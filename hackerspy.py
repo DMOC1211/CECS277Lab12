309ff9d9fbe551e09323aa05db9d3bf6df297fea
DMOC1211 <145091883+DMOC1211@users.noreply.github.com>
2026-04-22 12:28:31 -0700

    Update hackerspy.py

--- hackerspy.py
+++ hackerspy.py
@@ -4,7 +4,23 @@ Date: 4/22/26
 Group: 10
 Description:
 '''
+from spy import Spy
 
+
+class HackerSpy(Spy):
+    """Spy specializing in technology and hacking."""
+
+    def description(self):
+        return "Hacker Spy"
+
+    def agility(self):
+        return 2
+
+    def stealth(self):
+        return 3
+
+    def tech_ability(self):
+        return 5
 
 
 
